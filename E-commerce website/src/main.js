16c2d5759138603604fa2609edb2867c9bb8a2ec
Dulmi Chamathka <chamathkawijesekara23@gmail.com>
2024-12-23 08:25:29 +0530

    vue3 and vuetify 3 installation

---
+++ E-commerce website/src/main.js
@@ -0,0 +1,23 @@
+import "./assets/main.css";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "@fortawesome/fontawesome-free/css/all.css";
+
+const app = createApp(App);
+
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+app.use(router);
+app.use(vuetify);
+
+app.mount("#app");
